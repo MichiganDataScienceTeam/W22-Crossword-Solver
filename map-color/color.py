e31228d4bd7b914e90bea1b2f9965be6d0e84b05
aldencheung2 <aldencheung2@gmail.com>
2022-02-05 13:40:28 -0500

    Implemented colouring algorithm

--- map-color/color.py
+++ map-color/color.py
@@ -12,10 +12,35 @@ def read_file():
         adjacencies[b].add(a)
     return adjacencies
 
+def colour(graph, colourDict):
+    for node in graph:
+        print("try node " + node)
+        for i in range(3):
+            isValid = True
+            print("checking colour " + str(i))
+            for j in graph[node]:
+                print("Node adjacent node " + str(j))
+                if colourDict[j] == i:
+                    print(str(j) + " has colour " + str(colourDict[j])+ ", conflict")
+                    isValid = False
+                    break
+                else:
+                    print(str(j) + " has " + str(colourDict[j])+ ", no conflict")
+            if isValid:
+                colourDict[node] = i
+                print("colour " + node + " " + str(i))
+                break
+    # colour(graph, node, colourDict)
 
 def main():
     graph = read_file()
+
+    colourDict = {}
+    for node in graph:
+        colourDict[node] = -1
+    colour(graph, colourDict)
     print(graph)
+    print(colourDict)
 
 
 if __name__ == "__main__":
