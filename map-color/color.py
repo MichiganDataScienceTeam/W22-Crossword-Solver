2b4045fed44dfc79cfea66c57c318c8b330e8b89
Isabel Wang <bellwang11@gmail.com>
2022-02-12 12:31:05 -0500

    isabel map solution

--- map-color/color.py
+++ map-color/color.py
@@ -12,10 +12,33 @@ def read_file():
         adjacencies[b].add(a)
     return adjacencies
 
+def check(node, values, value, adjacencies):
+    for neighbor in adjacencies[node]:
+        if values[neighbor] == value:
+            return False
+    return True
+
+def try_this(node, values, adjacencies):
+    for i in range(1, 4):
+        if check(node, values, i, adjacencies):
+            values[node] = i
+            for neighbor in adjacencies[node]:
+                if values[neighbor] == 0:
+                    if not try_this(neighbor, values, adjacencies):
+                        return False
+    return values
+
+def brute(adjacencies):
+    values  = {}
+    for node in adjacencies:
+        values[node] = 0
+    for node in adjacencies:
+        print(try_this(node, values, adjacencies))
 
 def main():
     graph = read_file()
     print(graph)
+    brute(graph)
 
 
 if __name__ == "__main__":
